5a33283c6f83dad376a35efea2fd19412d8e3ac9
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-08-22 23:08:01 +0300

    Add project files.

---
+++ MongoDb/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDb.Entities;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string ProductId { get; set; }
+    public string Name { get; set; }
+    public int Stock { get; set; }
+    public decimal Price { get; set; }
+    [BsonRepresentation(BsonType.String)]
+    public string ImageUrl { get; set; }
+}
